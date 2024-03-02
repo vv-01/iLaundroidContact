bcf4c2787e98f80003780da145369ed3dcdaa531
vv-01 <asvijayavitthala@gmail.com>
2024-03-02 16:20:01 +0530

    vercel configurations added

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
+const nodemailer = require("nodemailer");
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.user,
+    pass: process.env.pass,
+  },
+});
 
 app.use(express.json());
 
-app.post("/api/send-email", (req, res) => {
-  const { to, subject, text } = req.body;
+app.post("/send-email", (req, res) => {
+  const { name, email, phone, message } = {
+    name: "A S Vijayavitthala",
+    email: "asvijayavitthala@gmail.com",
+    phone: "9481837702",
+    message: "hello testing1",
+  };
 
-  // Call the sendMail function here
+  const mailOptions = {
+    from: email,
+    to: "asvijayavitthala@gmail.com",
+    subject: `New message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+  };
+
+  console.log(mailOptions);
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent: " + info.response + mailOptions);
+      res.send("Email sent successfully");
+    }
+  });
 });
 
 app.listen(port, () => {
