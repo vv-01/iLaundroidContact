45b6d7659f17c3d93a79ca119853934a072bc005
vv-01 <asvijayavitthala@gmail.com>
2024-03-06 16:35:28 +0530

    dynamic data post method added

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Express JS on Vercel");
+  res.send("Express JS to post Data");
 });
 
 app.get("/ping", (req, res) => {
