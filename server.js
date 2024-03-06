9d493ceada28789da17ebdc5d0b0f7b6110a0491
vv-01 <asvijayavitthala@gmail.com>
2024-03-07 04:37:02 +0530

    recepient mail successfully configured

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.user,
-    pass: process.env.pass,
+    user: process.env.USER,
+    pass: process.env.PASS,
   },
 });
 
@@ -25,28 +25,36 @@ app.get("/ping", (req, res) => {
   res.send("pong 🏓");
 });
 
-app.post("/send-email", (req, res) => {
-  const { name, email, phone, message } = req.body;
-  console.log(res);
+app.post("/send-email", async (req, res) => {
+  const { name, email, phone, message, Rname, Remail, Rphone, Rmessage } =
+    req.body;
 
   const mailOptions = {
     from: email,
-    to: "asvijayavitthala@gmail.com",
+    to: "engineeringunicorns69@gmail.com", //laundry mail id
     subject: `New message from ${name}`,
     text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
   };
 
-  //   console.log(mailOptions);
+  const mailOptionsClients = {
+    from: "ilaundroidmangalore@gmail.com",
+    to: email,
+    subject: `New message from ${Rname}`,
+    text: `Name: ${Rname}\nEmail: ${Remail}\nPhone: ${Rphone}\nMessage: ${Rmessage}`,
+  };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send("Error sending email");
-    } else {
-      console.log("Email sent: " + info.response + mailOptions);
-      res.send("Email sent successfully");
-    }
-  });
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+
+    const infoClients = await transporter.sendMail(mailOptionsClients);
+    console.log("Client Email sent: " + infoClients.response);
+
+    res.send("Emails sent successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error sending emails");
+  }
 });
 
 app.listen(port, () => {
